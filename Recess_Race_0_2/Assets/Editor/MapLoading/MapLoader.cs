e5ccd6f3f697b2263a331c29490e378574cace49
Richard <rail.rich@gmail.com>
2014-02-06 20:18:36 -0500

    Start of the MapLoader

---
+++ Recess_Race_0_2/Assets/Editor/MapLoading/MapLoader.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MapLoader {
+
+    private static MapLoader instance = new MapLoader();
+    private MapLoader(){
+    }
+    public static void loadFromFile(string file)
+    {
+        string text = System.IO.File.ReadAllText(file);
+        instance.load(text);
+    }
+
+
+    private string[] lines;
+
+
+
+    private void load(string mapText)
+    {
+        GameObject.crea
+        lines = mapText.Split(new string[] { "\n\r", "\n", "\r" }, StringSplitOptions.None);
+        if (lines[0].StartsWith("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")) {
+            readLines();
+        } else {
+            Debug.LogError("Invalide File");
+        }
+	}
+
+    private void readLines() {
+        int lineIndex = getNextLayerLineIndex(0);
+        if (lineIndex != -1) {
+            Vector2 dimension = getLayerDimension(lines[lineIndex]);
+            lineIndex += 2;
+            int layerLineIndex = 0;
+            for (int fileLayerLineIndex = 0; fileLayerLineIndex < dimension.y; fileLayerLineIndex++) {
+                loadLayerLine(layerLineIndex, lines[fileLayerLineIndex]);
+                lineIndex++;
+                layerLineIndex++;
+            }
+
+            Debug.Log(lineIndex);
+        }
+    }
+
+    private void loadLayerLine(int layerLineIndex, string p)
+    {
+        throw new NotImplementedException();
+    }
+
+    private Vector2 getLayerDimension(string p)
+    {
+        int widthTextStart = p.IndexOf("width=");
+        int widthTextEnd = widthTextStart + 7;
+        int heightTextStart = p.IndexOf("height=");
+        int heightTextEnd = heightTextStart + 8;
+        int lineTextEnd = p.Length - 2;
+        int width = Int32.Parse(p.Substring(widthTextEnd, heightTextStart - 2 - widthTextEnd));
+        int height = Int32.Parse(p.Substring(heightTextEnd, lineTextEnd - heightTextEnd));
+        return new Vector2(width, height);
+    }
+
+    private int getNextLayerLineIndex(int startingIndex)
+    {
+        int index = startingIndex;
+        while (index < lines.Length) {
+            if (lines[index].StartsWith(" <layer name=")){
+                return index;
+            } else {
+                index++;
+            }
+            
+        }
+        return -1;
+    }
+
+}
---
+++ Recess_Race_0_2/Assets/Editor/MapLoading/MapLoaderEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class MapLoaderEditor : EditorWindow {
+
+	public string fileName = "";
+
+	void OnGUI(){
+		GUILayout.BeginHorizontal ();
+		fileName = GUILayout.TextField (fileName);
+		if (GUILayout.Button ("Find Map File")) {
+			fileName = EditorUtility.OpenFilePanel("Open Map file","../maps","tmx");
+		}
+		GUILayout.EndHorizontal ();
+		if (fileName.Length == 0) {
+			GUI.enabled = false;
+			GUILayout.Button("Load Map");
+			GUI.enabled = true;
+		}else{
+			if(GUILayout.Button("Load Map")) {
+                MapLoader.loadFromFile(fileName);
+			}
+		}
+	}
+	
+	
+	[MenuItem ("Window/MapLoader")]
+	public static void ShowWindow(){
+		EditorWindow.GetWindow(typeof(MapLoaderEditor), true, "MapLoader");
+	}
+}
