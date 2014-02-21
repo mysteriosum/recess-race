e7f4da478ca101c7cd2e85283d146dd8e6f8363e
Richard <rail.rich@gmail.com>
2014-02-21 11:36:36 -0500

    Added a lightout box system for jumping and other stuff debuging and testing

---
+++ Recess_Race_0_2/Assets/Recess Root/Scripts/LightoutBox.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightoutBox : MonoBehaviour {
+
+	private Color color = Color.yellow;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D other){
+		color = Color.green;
+	}
+
+	void OnDrawGizmos(){
+		Gizmos.color = color;
+		Gizmos.DrawCube (this.transform.position, Vector2.one);
+	}
+}
