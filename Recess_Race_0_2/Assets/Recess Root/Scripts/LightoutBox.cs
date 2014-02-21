8010f1fd86ba6a47eceb5ec7d2e4c1bca8952f6a
Richard <rail.rich@gmail.com>
2014-02-21 11:47:54 -0500

    Better Lighout

--- Recess_Race_0_2/Assets/Recess Root/Scripts/LightoutBox.cs
+++ Recess_Race_0_2/Assets/Recess Root/Scripts/LightoutBox.cs
@@ -4,21 +4,14 @@ using System.Collections;
 public class LightoutBox : MonoBehaviour {
 
 	private Color color = Color.yellow;
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
+
+
 	void Update () {
-	
+		GetComponent<SpriteRenderer>().color = color;
 	}
+
 
 	void OnTriggerEnter2D (Collider2D other){
 		color = Color.green;
-	}
-
-	void OnDrawGizmos(){
-		Gizmos.color = color;
-		Gizmos.DrawCube (this.transform.position, Vector2.one);
 	}
 }
