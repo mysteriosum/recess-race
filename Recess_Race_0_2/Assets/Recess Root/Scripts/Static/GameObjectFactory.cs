d11341834a62ec5a0943d81771ea02963bb6a621
Richard <rail.rich@gmail.com>
2014-02-24 21:00:03 -0500

    Support multiple tilesets

---
+++ Recess_Race_0_2/Assets/Recess Root/Scripts/Static/GameObjectFactory.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameObjectFactory  {
+
+	public static GameObject createGameObject(string name, GameObject parent = null){
+		GameObject newObject = new GameObject();
+		newObject.name = name;
+		if (parent != null) {
+			newObject.transform.parent = parent.transform;		
+		}
+		return newObject;
+	}
+
+	public static GameObject createCopyGameObject(GameObject original , string name, GameObject parent = null){
+		GameObject newObject = (GameObject)GameObject.Instantiate (original);
+		newObject.name = name;
+		if (parent != null) {
+			newObject.transform.parent = parent.transform;		
+		}
+
+		return newObject;
+	}
+}
