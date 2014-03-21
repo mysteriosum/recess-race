ffd18c1622ab8f3c69739a64c0fe4fd0e7c513c7
Travis Martin <travis.h.martin@gmail.com>
2014-03-20 20:16:49 -0400

    oops I forgot this file
    Signed-off-by: Travis Martin <travis.h.martin@gmail.com>

---
+++ Recess_Race_0_2/Assets/Recess Root/Scripts/Independent/StyleManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum StylePointTypes{
+	garbage,
+	style,
+	time,
+	rank,
+}
+
+public class StyleManager {
+
+	public int rankPoints = 0;
+	public int timePoints = 0;
+	public int garbagePoints = 0;
+	public int stylePoints = 0;
+}
