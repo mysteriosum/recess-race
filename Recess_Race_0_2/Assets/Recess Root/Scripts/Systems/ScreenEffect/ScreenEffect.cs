b2fa641b59675e1b514320e8283b14c7c89a8d91
Richard <rail.rich@gmail.com>
2014-05-22 17:20:25 -0400

    -Moved Stuff Added EffectSystem with FadeIn and FadeOut

---
+++ Recess_Race_0_2/Assets/Recess Root/Scripts/Systems/ScreenEffect/ScreenEffect.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class ScreenEffect {
+
+	public bool done;
+	protected float life;
+
+	public delegate void DoneAction();
+	public event DoneAction OnDone;
+
+	public ScreenEffect(float lifetime){
+		this.life = lifetime;
+	}
+
+	public void OnGui(float time){
+		if (done) return;
+		life -= time;
+		if (life <= 0) {
+			setDone();
+		}
+		show();			
+	}
+
+	private void setDone(){
+		done = true;
+		if (OnDone != null) {
+			OnDone ();		
+		}
+
+	}
+	abstract public void show();
+}
